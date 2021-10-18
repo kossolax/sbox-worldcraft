c92ebde2558e1e1dad1a33a015b400c5616b4118
Matt Stevens <matt@mattstevens.co.uk>
2021-10-18 15:17:44 +0100

    Selection tool, just show an outline for now and press F to delete primitives

---
+++ code/Tools/SelectionTool.cs
@@ -0,0 +1,36 @@
+﻿using Sandbox;
+
+namespace WorldCraft
+{
+	[Library( "tool_select", Title = "selection", Icon = "SelectionTool.png" )]
+	public partial class SelectionTool : BaseTool
+	{
+		// TODO: [Net] on this kills it.
+		public PrimitiveEntity SelectedEntity { get; set; }
+
+		public override void Simulate( Client cl )
+		{
+			var tr = Trace.Ray( Input.Cursor, 5000.0f ).Run();
+
+			if ( Input.Pressed( InputButton.Attack1 ) )
+			{
+				SelectedEntity = tr.Entity as PrimitiveEntity;
+			}
+
+			if ( Input.Pressed( InputButton.Flashlight ) && Host.IsServer )
+			{
+				SelectedEntity?.Delete();
+			}
+		}
+
+		public override void FrameSimulate( Client cl )
+		{
+			if ( SelectedEntity.IsValid() )
+			{
+				var bbox = SelectedEntity.CollisionBounds;
+				DebugOverlay.Box( 0, SelectedEntity.Position, SelectedEntity.Rotation, bbox.Mins, bbox.Maxs, Color.Yellow );
+				DebugOverlay.Axis( SelectedEntity.Position, SelectedEntity.Rotation, 16, depthTest: false );
+			}
+		}
+	}
+}
