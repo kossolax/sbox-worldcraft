3b48301472d64cfe20c7ec4483cf95411db16c26
Matt Stevens <matt@mattstevens.co.uk>
2021-10-18 22:55:55 +0100

    Use Planar UV mapping when creating a box primitive.

--- code/Primitives/PrimitiveBox.cs
+++ code/Primitives/PrimitiveBox.cs
@@ -1,21 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
 using Sandbox;
 
 namespace WorldCraft
 {
 	public partial class PrimitiveBox : BasePrimitive
 	{
+		[Net] public Vector3 Origin { get; set; }
 		[Net, Change( nameof( DirtyEntity ) )] public Vector3 Size { get; set; }
 
 		public override Model BuildModel()
 		{
 			var mesh = new Mesh( Material.Load( "materials/dev/reflectivity_30.vmat" ) );
-
-			var vb = new VertexBuffer();
-			vb.Init( true );
-
-			vb.AddCube( Vector3.Zero, Size, Rotation.Identity );
-
-			mesh.CreateBuffers( vb );
+			BuildMesh( mesh );
 
 			var model = Model.Builder
 				.AddMesh( mesh )
@@ -23,6 +20,94 @@ namespace WorldCraft
 				.Create();
 
 			return model;
+		}
+
+		private static Vector2 Planar( Vector3 pos, Vector3 uAxis, Vector3 vAxis )
+		{
+			return new Vector2()
+			{
+				x = Vector3.Dot( uAxis, pos ),
+				y = Vector3.Dot( vAxis, pos )
+			};
+		}
+
+		public override void BuildMesh( Mesh mesh )
+		{
+			var positions = new Vector3[]
+			{
+				new Vector3(-0.5f, -0.5f, 0.5f) * Size,
+				new Vector3(-0.5f, 0.5f, 0.5f) * Size,
+				new Vector3(0.5f, 0.5f, 0.5f) * Size,
+				new Vector3(0.5f, -0.5f, 0.5f) * Size,
+				new Vector3(-0.5f, -0.5f, -0.5f) * Size,
+				new Vector3(-0.5f, 0.5f, -0.5f) * Size,
+				new Vector3(0.5f, 0.5f, -0.5f) * Size,
+				new Vector3(0.5f, -0.5f, -0.5f) * Size,
+			};
+
+			var faceIndices = new int[]
+			{
+				0, 1, 2, 3,
+				7, 6, 5, 4,
+				0, 4, 5, 1,
+				1, 5, 6, 2,
+				2, 6, 7, 3,
+				3, 7, 4, 0,
+			};
+
+			var uAxis = new Vector3[]
+			{
+				Vector3.Forward,
+				Vector3.Left,
+				Vector3.Left,
+				Vector3.Forward,
+				Vector3.Right,
+				Vector3.Backward,
+			};
+
+			var vAxis = new Vector3[]
+			{
+				Vector3.Left,
+				Vector3.Forward,
+				Vector3.Down,
+				Vector3.Down,
+				Vector3.Down,
+				Vector3.Down,
+			};
+
+			List<SimpleVertex> verts = new();
+			List<int> indices = new();
+
+			for ( var i = 0; i < 6; ++i )
+			{
+				var tangent = uAxis[i];
+				var binormal = vAxis[i];
+				var normal = Vector3.Cross( tangent, binormal );
+
+				for ( var j = 0; j < 4; ++j )
+				{
+					var vertexIndex = faceIndices[(i * 4) + j];
+					var pos = positions[vertexIndex];
+
+					verts.Add( new SimpleVertex()
+					{
+						position = pos,
+						normal = normal,
+						tangent = tangent,
+						texcoord = Planar( (Origin + pos) / 32, uAxis[i], vAxis[i] )
+					} );
+				}
+
+				indices.Add( i * 4 + 0 );
+				indices.Add( i * 4 + 2 );
+				indices.Add( i * 4 + 1 );
+				indices.Add( i * 4 + 2 );
+				indices.Add( i * 4 + 0 );
+				indices.Add( i * 4 + 3 );
+			}
+
+			mesh.CreateVertexBuffer<SimpleVertex>( verts.Count, SimpleVertex.Layout, verts.ToArray() );
+			mesh.CreateIndexBuffer( indices.Count, indices.ToArray() );
 		}
 	}
 }
